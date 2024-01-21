93173f96292f2cd5c696d4ebe0dea1cfcbcd438c
arifozden <arifozden1@gmail.com>
2024-01-22 00:15:39 +0100

    intro02

--- smartEd_con/pages/urls.py
+++ smartEd_con/pages/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('courses/', views.courses, name='courses'),
+    path('teachers/', views.teachers, name='teachers'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
     ]
--- smartEd_con/pages/views.py
+++ smartEd_con/pages/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html'),
+
+def about(request):
+    return render(request, 'about.html'),
+
+def courses(request):
+    return render(request, 'courses.html'),
+
+def teachers(request):
+    return render(request, 'teachers.html'),
+
+def contact(request):
+    return render(request, 'contact.html'),
+
+def login(request):
+    return render(request, 'login.html'),
+
+def register(request):
+    return render(request, 'register.html'),
